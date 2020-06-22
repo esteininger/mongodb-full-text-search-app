06f6b9f40d0ef045e860e2022a13c7878cefb492
Ethan Steininger <esteininger21@gmail.com>
2020-06-22 10:21:12 -0400

    no message

---
+++ README.md
@@ -0,0 +1,4 @@
+# mongodb-full-text-search-app
+Demonstration of MongoDB's Full Text Search capability within a Flask web application
+
+Create a `config.py` file with your `mongo_uri` connection string.
--- manage.py
+++ manage.py
@@ -2,21 +2,23 @@ from flask import Flask, render_template, request, jsonify
 import os
 import pymongo
 import ssl
+# relaxed JSON
+from bson import json_util
+from config import mongo_uri
 
-full_url = "mongodb://127.0.0.1:27017/clintrials"
 
 app = Flask(__name__)
 
 # connection obj
-conn = pymongo.MongoClient(full_url, ssl_cert_reqs=ssl.CERT_NONE)
+conn = pymongo.MongoClient(mongo_uri, ssl_cert_reqs=ssl.CERT_NONE)
 # collection
-collection = conn['clintrials']['clinical_studies']
+collection = conn['sample_mflix']['movies']
 
 # endpoint
 @app.route('/search', methods=['GET'])
 def search():
-    query = request.args.get('q', default=None, type=str)
-    path = request.args.get('f', default='brief_title', type=str)
+    query = request.args.get('query', default=None, type=str)
+    path = request.args.get('path', default='title', type=str)
 
     agg_pipeline = [
         {
@@ -32,20 +34,27 @@ def search():
             }
         }, {
             '$project': {
-                '_id': 0,
-                'brief_title': 1,
-                'source': 1,
+                # include entire document
+                'document': "$$ROOT",
+                # include score
                 'score': {
                     '$meta': 'searchScore'
                 }
             }
-        }, {
-            '$limit': 15
+        },
+        {
+            '$replaceRoot': {'newRoot': "$document"}
+        },
+        {
+            '$limit': 10
+        },
+        {
+            '$sort': {'score': -1}
         }
     ]
-    results = list(collection.aggregate(agg_pipeline))
-    # json_result = json_util.dumps({'docs': docs}, json_options=RELAXED_JSON_OPTIONS)
-    return jsonify(results)
+    docs = list(collection.aggregate(agg_pipeline))
+    json_result = json_util.dumps({'docs': docs}, json_options=json_util.RELAXED_JSON_OPTIONS)
+    return jsonify(json_result)
 
 # page
 @app.route('/')
